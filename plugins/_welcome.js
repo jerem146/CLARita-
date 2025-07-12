8a865a2262a5df179dfec6685f01ab2ed91fa62a
jerem146 <vasquezever146@gmail.com>
2025-07-11 23:04:10 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,25 +3,62 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
-  let img = await (await fetch(`${pp}`)).buffer()
-  let chat = global.db.data.chats[m.chat]
-  let txt = 'ゲ◜៹ New Member ៹◞ゲ'
-  let txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
-  let groupSize = participants.length
-  if (m.messageStubType == 27) {
-    groupSize++;
-  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
-    groupSize--;
+
+  const fkontak = {
+    key: {
+      participants: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast",
+      fromMe: false,
+      id: "Halo"
+    },
+    message: {
+      contactMessage: {
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+      }
+    },
+    participant: "0@s.whatsapp.net"
   }
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
-    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
+  const chat = global.db.data.chats[m.chat]
+  const who = m.messageStubParameters[0]
+  const username = `@${who.split('@')[0]}`
+  const groupSize = m.messageStubType == 27
+    ? participants.length + 1
+    : (m.messageStubType == 28 || m.messageStubType == 32)
+    ? participants.length - 1
+    : participants.length
+
+  const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
+  const img = await (await fetch(pp)).buffer()
+
+  const defaultWelcome = 'Bienvenido/a 😺'
+  const defaultBye = 'Se fue del grupo...'
+
+  const welcomeMsg = chat.welcomeMessage || global.welcom1 || defaultWelcome
+  const byeMsg = chat.despMessage || global.welcom2 || defaultBye
+
+  const txt = 'ゲ◜៹ New Member ៹◞ゲ'
+  const txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
+
+  if (chat.welcome && m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+    let bienvenida = `┏╼★ Bienvenido
+┋ 「 ${username} 」
+┗╼★ 「 ${groupMetadata.subject} 」
+┋❖ ${welcomeMsg}
+┋❀ Miembros actuales: ${groupSize}
+┗━━━━━━━━━━━━━━━┅ ⳹
+> ✐ Usa *#help* para ver mis comandos.`
+    await conn.sendMini(m.chat, txt, global.dev, bienvenida, img, img, global.redes, fkontak)
   }
-  
-  if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
-    await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
-  }}
+
+  if (chat.welcome && (m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_LEAVE)) {
+    let bye = `┏╼★ Adiós
+┋ 「 ${username} 」
+┗╼★ 「 ${groupMetadata.subject} 」
+┋❖ ${byeMsg}
+┋❀ Miembros actuales: ${groupSize}
+┗━━━━━━━━━━━━━━━┅ ⳹
+> > ${global.dev}`
+    await conn.sendMini(m.chat, txt1, global.dev, bye, img, img, global.redes, fkontak)
+  }
+}
